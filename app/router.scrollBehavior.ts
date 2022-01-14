0c39fe256d10496633b6b2aea5d661dc937577b7
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-01-14 21:46:02 +0900

    Create router.scrollBehavior.ts

---
+++ app/router.scrollBehavior.ts
@@ -0,0 +1,26 @@
+import { Route } from 'vue-router/types/router'
+
+type PositionDescriptor =
+  { x: number, y: number } |
+  { selector: string } |
+  void
+
+type scrollBehaviorHandler = (
+  to: Route,
+  from: Route,
+  savedPosition?: { x: number, y: number }
+) => PositionDescriptor | Promise<PositionDescriptor>
+
+const scrollBehavior:scrollBehaviorHandler = (to, _from, savedPosition) => {
+  if (savedPosition) {
+    return savedPosition
+  }
+  else if (to.hash) {
+    return { selector: to.hash }
+  }
+  else {
+    return { x: 0, y: 0 }
+  }
+}
+
+export default scrollBehavior
