6ee53b0b43555f9f3726404992af222f8b5ba69e
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-01-18 00:15:17 +0900

    Create error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,27 @@
 <template>
-  <h1>{{ error.statusCode }}</h1>
+  <div>
+    <h1>{{ error.statusCode }}</h1>
+    <p>{{ statuscode2message(error.statusCode) }}</p>
+    <nuxt-link to="/">{{ $t('トップページに戻る') }}</nuxt-link>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import StatusCode from '~/assets/status-code.json'
 export default Vue.extend({
   props: {
     error: {
       type: Object,
       default: () => ({}),
+    },
+  },
+  methods: {
+    statuscode2message(statusCode: number) {
+      return (
+        (StatusCode as { [key: string]: string })[String(statusCode)] ||
+        'Unknown Error'
+      )
     },
   },
 })
