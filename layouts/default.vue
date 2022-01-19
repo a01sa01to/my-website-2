6c1d3af35822e09e963c16460e2258d9d5d50a6b
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-01-19 15:46:06 +0900

    Impl darkmode detection

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>
-    <header><SharedHeader /></header>
+  <div :data-darkmode="String(isDarkmode)">
+    <SharedHeader />
     <main><Nuxt /></main>
-    <footer><SharedFooter /></footer>
+    <SharedFooter />
+    <div id="darkmode-dummy" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+main {
+  margin-top: calc(64px + 1em);
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      isDarkmode: false,
+    }
+  },
+  mounted() {
+    const darkmodeDOM = document.getElementById('darkmode-dummy')
+    if (darkmodeDOM) {
+      const observer = new IntersectionObserver(() => {
+        this.isDarkmode = getComputedStyle(darkmodeDOM).display === 'block'
+      })
+      observer.observe(darkmodeDOM)
+    }
+  },
+})
+</script>
