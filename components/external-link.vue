cd17c6fffe640af17c45b8788644df0ff267b92d
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-01-22 22:46:00 +0900

    Create external-link.vue

---
+++ components/external-link.vue
@@ -0,0 +1,21 @@
+<template>
+  <a :href="to" target="_blank" rel="noopener noreferrer">
+    <slot />
+    <b-icon-box-arrow-up-right />
+  </a>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { BIconBoxArrowUpRight } from 'bootstrap-vue'
+export default Vue.extend({
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    BIconBoxArrowUpRight,
+  },
+})
+</script>
