a490ad06ab5bb3b4a9501bf09de33c4bf5de1353
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-02-07 14:07:47 +0900

    Fix blog/index.vue: b-containerの閉じタグがおかしい

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -9,32 +9,32 @@
         <template #lead>Scroll down to explore this site</template>
       </b-jumbotron>
       <scroll-next-animation />
-      <b-container v-if="$i18n.locale !== 'ja'">
-        <h2>Caution</h2>
-        <p>
-          For the sake of translation effort, this blog is available in Japanese
-          only.
-        </p>
-      </b-container>
-      <b-container>
-        <h2>Latest Articles</h2>
-        <b-row cols-xl="3" cols-lg="3" cols-md="2" cols-sm="1" cols="1">
-          <b-col v-for="(val, key) in BlogList" :key="key">
-            <BlogCard :article="val" />
-          </b-col>
-        </b-row>
-      </b-container>
-      <b-container>
-        <h2>Find more...</h2>
-        <ul>
-          <li>
-            <nuxt-link :to="`/blog/${thisYear}/`"
-              >{{ thisYear }}年の記事一覧</nuxt-link
-            >
-          </li>
-          <li><nuxt-link to="/blog/tags/">タグ一覧</nuxt-link></li>
-        </ul>
-      </b-container>
+    </b-container>
+    <b-container v-if="$i18n.locale !== 'ja'">
+      <h2>Caution</h2>
+      <p>
+        For the sake of translation effort, this blog is available in Japanese
+        only.
+      </p>
+    </b-container>
+    <b-container>
+      <h2>Latest Articles</h2>
+      <b-row cols-xl="3" cols-lg="3" cols-md="2" cols-sm="1" cols="1">
+        <b-col v-for="(val, key) in BlogList" :key="key">
+          <BlogCard :article="val" />
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-container>
+      <h2>Find more...</h2>
+      <ul>
+        <li>
+          <nuxt-link :to="`/blog/${thisYear}/`"
+            >{{ thisYear }}年の記事一覧</nuxt-link
+          >
+        </li>
+        <li><nuxt-link to="/blog/tags/">タグ一覧</nuxt-link></li>
+      </ul>
     </b-container>
   </div>
 </template>
