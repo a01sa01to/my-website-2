2c4c7782d56b735e7b667f5c0b7f9ae564bab052
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-02-08 18:12:12 +0900

    Create opendata/index.vue

---
+++ pages/opendata/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <b-container>
+      <b-jumbotron
+        :text-variant="$store.state.darkmode ? 'light' : 'dark'"
+        :bg-variant="$store.state.darkmode ? 'dark' : 'light'"
+      >
+        <template #header>Asa's Opendata Catalog</template>
+        <template #lead>Scroll down to explore this site</template>
+      </b-jumbotron>
+      <scroll-next-animation />
+    </b-container>
+    <b-container>
+      <h2>About</h2>
+      <p>
+        {{
+          $t(
+            'このサイトは、Asaがオープンデータを提供するために立ち上げたサイトです。'
+          )
+        }}<br />
+        {{
+          $t(
+            '公式には提供されていないデータを、まとめて提供しています。ファイル形式はCSVやJSONを用紙しています。'
+          )
+        }}
+      </p>
+    </b-container>
+    <b-container>
+      <h2>What is Open data?</h2>
+      <p>
+        {{
+          $t(
+            'オープンデータとは、営利目的、非営利目的を問わず二次利用可能なルールが適用され、機械判読に適しており、無償で利用できるデータのことです。'
+          )
+        }}<br />
+        {{
+          $t(
+            '当サイトで公開しているデータの情報の正確性については万全を期していますが、利用者が当サイトの情報を用いて行う一切の行為について責任を負うものではありません。'
+          )
+        }}
+      </p>
+    </b-container>
+    <b-container>
+      <h2>Available Resources</h2>
+      <ul>
+        <li>
+          <nuxt-link to="./covid19-ibaraki/">
+            {{ $t('茨城県新型コロナウイルス感染症') }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </b-container>
+    <b-container>
+      <h2>Available API</h2>
+      <p>
+        {{
+          $t(
+            'このサイトでは、GraphQLを用い、APIを提供しています。利用方法については、以下のページをご覧ください。'
+          )
+        }}<br />
+        <nuxt-link to="/opendata/api-usage">
+          {{ $t('当サイトのOpendata APIの使い方') }}
+        </nuxt-link>
+      </p>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import toDate from '~/utils/date_format'
+
+export default Vue.extend({
+  async asyncData({ $content }) {
+    const BlogList = await $content('articles', { deep: true })
+      .sortBy('createDate', 'desc')
+      .limit(3)
+      .fetch()
+    return {
+      BlogList,
+    }
+  },
+  head: {
+    title: 'Opendata',
+  },
+  data() {
+    return {
+      thisYear: new Date().getFullYear(),
+      toDate,
+    }
+  },
+})
+</script>
