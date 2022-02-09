58a1f0212e3bed179673bd2794ac8cc64e0a6c67
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-02-09 11:45:00 +0900

    Create opendata/covid19-ibaraki/death-attributes.vue

---
+++ pages/opendata/covid19-ibaraki/death-attributes.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <shared-main-header :title="$t('死亡者属性')" :b_data="breadcrumb_data" />
+    <b-container>
+      <p>
+        {{
+          $t(
+            '茨城県内の、新型コロナウイルスによる死亡者の属性を集計したオープンデータです。'
+          )
+        }}
+      </p>
+    </b-container>
+    <b-container>
+      <h2>{{ $t('ファイルに関する情報') }}</h2>
+      <opendata-metadata
+        :last_update="last_update"
+        created_date="2020/05/23 16:00"
+        file_name="080004_ibaraki_covid19_death_attributes.csv"
+        :file_size="size"
+        data_format="csv"
+      />
+    </b-container>
+    <b-container>
+      <h2>{{ $t('ご利用時の注意点') }}</h2>
+      <ul>
+        <li>
+          {{
+            $t(
+              '当データの情報の正確性については万全を期していますが、利用者が当サイトの情報を用いて行う一切の行為について責任を負うものではありません。'
+            )
+          }}
+        </li>
+        <li>
+          {{
+            $t(
+              '当データは、あくまで県の公式情報をもとに作成したものです。県によって公式に公開されているものではありません。'
+            )
+          }}
+        </li>
+        <li>
+          {{
+            $t(
+              'CSVファイルをダウンロードした際にExcelで表示すると、文字化けが起きたり、列が異なったりすることがあります。その際には、別のソフトウェアをご利用ください。'
+            )
+          }}
+        </li>
+      </ul>
+    </b-container>
+    <b-container>
+      <h2>{{ $t('ダウンロード') }}</h2>
+      <p>
+        {{
+          $t(
+            '以下のボタンからダウンロードできます。また、以下URLからcURL等の各種ライブラリによってダウンロードすることも可能です。'
+          )
+        }}
+      </p>
+      <opendata-url-dl-copy
+        url="https://a01sa01to.com/opendata/api/raw/covid19_ibaraki/080004_ibaraki_covid19_death_attributes.csv"
+        file_name="080004_ibaraki_covid19_call_center.csv"
+      />
+    </b-container>
+    <b-container>
+      <h2>{{ $t('Opendata APIについて') }}</h2>
+      <p>
+        {{
+          $t(
+            '直接ダウンロードするほか、当サイトが提供するOpendata APIもご利用いただけます。'
+          )
+        }}
+        <nuxt-link to="/opendata/api-usage/">
+          {{ $t('Opendata APIの使い方についてはこちら') }}
+        </nuxt-link>
+      </p>
+      <div class="nuxt-content-highlight">
+        <pre class="language-text"><code>query {
+  covid19_ibaraki {
+    death_attributes( [PAGINATION INFO] ) {
+      dataset {
+        no               <span class="token comment"># {{$t('死亡者の通し番号')}}: Int!</span>
+        government_code  <span class="token comment"># {{$t('全国地方公共団体コード')}}: String!</span>
+        prefecture       <span class="token comment"># {{$t('都道府県名')}}: String!</span>
+        city             <span class="token comment"># {{$t('市区町村名')}}: String</span>
+        date             <span class="token comment"># {{$t('公表日')}}: String!</span>
+        age              <span class="token comment"># {{$t('年代')}}: String!</span>
+        gender           <span class="token comment"># {{$t('性別')}}: String!</span>
+
+      }
+      pageinfo {
+        hasPreviousPage  <span class="token comment"># {{$t('前のページがあるかどうか')}}: Boolean!</span>
+        hasNextPage      <span class="token comment"># {{$t('次のページがあるかどうか')}}: Boolean!</span>
+        startCursor      <span class="token comment"># {{$t('現在のページの最初のCursor')}}: String!</span>
+        endCursor        <span class="token comment"># {{$t('現在のページの最後のCursor')}}: String!</span>
+      }
+      last_update  <span class="token comment"># {{$t('最終更新日時')}}: String!</span>
+    }
+  }
+}</code></pre>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import FileData from '~/data/opendata/covid19_ibaraki.json'
+
+export default Vue.extend({
+  head() {
+    return {
+      title: `${this.$t('死亡者属性')} - ${this.$t(
+        '茨城県新型コロナウイルス感染症'
+      )} - Opendata`,
+    }
+  },
+  data() {
+    return {
+      breadcrumb_data: [
+        { to: '/', text: 'Home' },
+        { to: '/opendata/', text: 'Opendata' },
+        {
+          to: '/opendata/covid19-ibaraki/',
+          text: this.$t('茨城県新型コロナウイルス感染症'),
+        },
+        {
+          to: '/opendata/covid19-ibaraki/call-center/',
+          text: this.$t('死亡者属性'),
+          active: true,
+        },
+      ],
+      last_update: FileData.death_attr.lastUpdate,
+      size: FileData.death_attr.size,
+    }
+  },
+})
+</script>
