9ff27f61873ae73bbc48d3a93f8b66caac82468e
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-02-17 11:07:55 +0900

    Update twttr_load.js: varをletにする

--- static/twttr_load.js
+++ static/twttr_load.js
@@ -1,9 +1,8 @@
 window.twttr = (function (d, s, id) {
-  var js,
+  let js = d.createElement(s),
     fjs = d.getElementsByTagName(s)[0],
     t = window.twttr || {}
   if (d.getElementById(id)) return t
-  js = d.createElement(s)
   js.id = id
   js.src = 'https://platform.twitter.com/widgets.js'
   fjs.parentNode.insertBefore(js, fjs)
