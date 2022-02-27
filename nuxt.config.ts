7b4c217c0a3c6ed4fdc182d81c954b186b11a1c6
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-02-27 16:44:59 +0900

    Update nuxt.config.ts: Opendata APIのエンドポイントをリダイレクトさせる

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -121,7 +121,16 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     [
       '@nuxtjs/redirect-module',
-      [{ from: '^/blog/(.*)$', to: '/uconst/', statuscode: 302 }],
+      [
+        { from: '^/blog/(.*)$', to: '/uconst/', statusCode: 302 },
+        {
+          // POSTリクエスト対応のため、301ではなく308にする
+          // Ref: https://developer.mozilla.org/ja/docs/Web/HTTP/Status/308
+          from: '^/opendata/api/(.*)$',
+          to: '/api/opendata/$1',
+          statusCode: 308,
+        },
+      ],
     ],
   ],
 
