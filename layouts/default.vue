1a219fdead0e87645518ea768b16691e01bc2ffe
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-04 00:22:14 +0900

    Update default.vue: Loaderを追加（#75）

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,33 @@
 <template>
   <div>
+    <div id="loader" :data-loading="loading" v-if="showing">
+      <b-spinner variant="primary" class="m-2" />
+      <span>Loading...</span>
+    </div>
     <shared-header />
     <main><nuxt /></main>
     <shared-footer />
   </div>
 </template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      loading: true,
+      showing: true,
+    }
+  },
+  mounted() {
+    this.loading = false
+    setTimeout(() => {
+      this.showing = false
+    }, 3000)
+  },
+})
+</script>
 
 <style lang="scss" scoped>
 div {
@@ -15,5 +38,35 @@ div {
 main {
   flex: 1;
   padding-top: calc(64px + 1rem);
+}
+
+div#loader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #f8f9fa;
+  z-index: 9999;
+  color: #343a40;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  transition-property: opacity;
+  transition-duration: 1s;
+  transition-delay: 1.5s;
+  &[data-loading='true'] {
+    opacity: 1;
+  }
+  span {
+    font-size: 1.5rem;
+  }
+}
+@media (prefers-color-scheme: dark) {
+  div#loader {
+    background-color: #343a40;
+    color: #f8f9fa;
+  }
 }
 </style>
