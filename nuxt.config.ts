d265b735c466c932c37a66c43c4997c73ec7b203
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-09 21:52:21 +0900

    Fix nuxt.config.ts: Faviconの相対パスがおかしかった

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
     link: [
       {
         rel: 'icon',
-        href: 'favicon.png',
+        href: '/favicon.png',
         type: 'image/png',
       },
       {
