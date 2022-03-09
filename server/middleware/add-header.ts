5db6f7fcc7e07b5dd2810b38721262b5853e3609
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-09 21:43:48 +0900

    Update add-header.ts: CSPを追加

--- server/middleware/add-header.ts
+++ server/middleware/add-header.ts
@@ -1,5 +1,16 @@
 import type { IncomingMessage, ServerResponse } from 'http'
 
+const csp = [
+  "default-src 'self'",
+  "child-src 'self' https://platform.twitter.com",
+  "font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net",
+  "frame-src 'self' https://platform.twitter.com",
+  "img-src 'self' https://i.creativecommons.org/l/by-sa/4.0/88x31.png https://licensebuttons.net/l/by-sa/4.0/88x31.png https://opengraph.githubassets.com",
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://platform.twitter.com",
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net/npm/katex@latest/dist/katex.min.css",
+  'report-uri /api/csp-report/',
+]
+
 export default (
   _req: IncomingMessage,
   res: ServerResponse,
@@ -12,5 +23,6 @@ export default (
     )
   }
   res.setHeader('X-Content-Type-Options', 'nosniff')
+  res.setHeader('Content-Security-Policy', csp.join('; '))
   next()
 }
